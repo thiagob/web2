72a260a686afecd9881e5a6a20ba493cfb9fa495
thiagob <thiagob@gmail.com>
2022-08-08 22:12:23 -0300

    Hello World with Express

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const http = require('http')
-
-const hostname = '127.0.0.1'
+const express = require('express')
+const app = express()
 const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-
-  html = `
-  <html>
-    <head>
-      <script src="jquery-3.6.0.min.js"></script>
-    </head>
-    <body>
-      <h1>Hello World</h1>
-    </body>
-  </html>
-  `
-
-  res.end(html)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
