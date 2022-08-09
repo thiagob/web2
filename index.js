d8a39caa74c41eaf9714e524d7208c1921a0e90c
thiagob <thiagob@gmail.com>
2022-08-08 22:45:43 -0300

    Adiciona comentários e redirecionamento.

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  // retornará Hello World!
   res.send('Hello World!')
+})
+
+app.get('/google', (req, res) => {
+  // retornará http status 302 e redicionará o browser para o Google
+  res.redirect('http://www.google.com');
 })
 
 app.listen(port, () => {
