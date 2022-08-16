23f6cb057497a28357c741b38bf51122047d3cc9
thiagob <thiagob@gmail.com>
2022-08-15 22:29:40 -0300

    Adiciona body-parser

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
   // retornará Hello World!
