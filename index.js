8d7d6bf88e09481ad1af6cf74c1ba8ea866ac782
thiagob <thiagob@gmail.com>
2022-08-15 22:31:37 -0300

    Implementa GET e POST da API de Alunos

--- index.js
+++ index.js
@@ -28,12 +28,44 @@ app.get('/google', (req, res) => {
 
 // lista os alunos 
 app.get('/api/alunos', (req, res) => {
+  // adiciona o cabeçalho content-type = text/json
+  res.type('json')
+  // retorna relação de alunos
+  res.send(JSON.stringify({
+    alunos: alunos
+  }));
 });
 
 // cria novo aluno
 app.post('/api/alunos', (req, res) => {
 
+  let errors = []
   
+  // valida se o nome foi informado
+  if (!("nome" in req.body) || req.body.nome.length == 0) {
+    errors.push('Nome não informado.')
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({
+      errors: errors
+    });
+  }
+
+  // prepara aluno para adição
+  let aluno = {
+    "id": alunos.length + 1,
+    "nome": req.body.nome,
+    "curso": req.body.curso
+  }
+
+  // adiciona aluno no array de alunos
+  alunos.push(aluno)
+
+  // retorna aluno criado
+  res.status(200).json({
+    aluno: aluno
+  });
 })
 
 app.listen(port, () => {
